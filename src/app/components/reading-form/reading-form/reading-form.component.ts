036d1995696cf26fcfad3be01df9675530b691de
quinise <quinise.ercolano@gmail.com>
2025-04-10 14:01:31 -0700

    committing staged files

--- src/app/components/reading-form/reading-form/reading-form.component.ts
+++ src/app/components/reading-form/reading-form/reading-form.component.ts
@@ -5,15 +5,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialog, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTimepickerModule } from '@angular/material/timepicker';
+import { Router } from '@angular/router';
 import { BookService } from 'src/app/services/book.service';
 
+
 interface ReadingForm {
-  id: FormControl<string>;
   name: FormControl<string>;
   email: FormControl<string>;
   phone_number: FormControl<number | null>;
@@ -44,19 +45,26 @@ interface ReadingForm {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReadingFormComponent {
+  serviceIdNumber: number;
+
   _todaysDate = new Date();
   minDate = new Date();
   maxDate = new Date();
 
-  constructor (private _matDialog:MatDialog, @Inject(MAT_DIALOG_DATA) public data: {serviceType: string}, public bookService: BookService) {
+  constructor (private _matDialog:MatDialog, @Inject(MAT_DIALOG_DATA) public data: {serviceType: string}, public bookService: BookService, private router: Router) {
+    this.serviceIdNumber = this.generateIdNumber();
+
     this.minDate.setDate(this._todaysDate.getDate() + 2);
     this.maxDate.setMonth(this._todaysDate.getMonth() + 2);
   }
 
+  generateIdNumber(): number {
+    const min = 10000000;
+    const max = 99999999;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   protected readingForm = new FormGroup<ReadingForm>({
-    id: new FormControl<string>('', {
-      nonNullable: true,
-    }),
     name: new FormControl<string>('', {
       nonNullable: true,
       validators: [Validators.pattern(/[a-zA-Z ]/), Validators.required]
@@ -82,6 +90,19 @@ export class ReadingFormComponent {
     }),
   })
 
+  submitFakeData() {
+    console.log('submitting fake data');
+    // this.bookService.setServiceDetails(
+    //   123456789, 
+    //   "Reading", 
+    //   'I am a client', 
+    //   'my@email.com', 
+    //   1231231234, 
+    //   new Date(), 
+    //   true,
+    // );
+  }
+
   onSubmit(form: any): void {
     const combinedDateTime = new Date(
       form.value.date.getFullYear(),
@@ -91,7 +112,27 @@ export class ReadingFormComponent {
       form.value.time.getMinutes(),
       form.value.time.getSeconds(),
     )
-    this.bookService.setServiceDetails(this.data.serviceType, form.value.name, form.value.email, form.value.phone_number, combinedDateTime, form.value.time, form.value.isVirtual);
-    console.log(`TESTING: Form Values in Form ${form.value}`);
+
+    this.bookService.service = {
+      id: this.serviceIdNumber,
+      type: this.data.serviceType,
+      name: form.value.name,
+      email: form.value.email,
+      phone_number: form.value.phone_number,
+      date: combinedDateTime,
+      isVirtual: form.value.isVirtual
+     };
+    this.router.navigate(['/events']);
+
+    // this.bookService.setServiceDetails(
+    // this.serviceIdNumber, 
+    // this.data.serviceType, 
+    // form.value.name, 
+    // form.value.email, 
+    // form.value.phone_number, 
+    // combinedDateTime, 
+    // form.value.isVirtual);
+
+    // localStorage.setItem('requestedService', JSON.stringify(this.bookService.getAllServicesDetails()))
   }
 }
--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, inject } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { Service } from 'src/app/interfaces/service';
 import { BookService } from 'src/app/services/book.service';
+import { ReadingFormComponent } from 'src/app/components/reading-form/reading-form/reading-form.component';
+// import { Router } from '@angular/router';
 @Component({
     selector: 'app-events',
     imports: [FullCalendarModule, CommonModule],
@@ -15,21 +17,25 @@ import { BookService } from 'src/app/services/book.service';
 })
 
 export class EventsComponent {
-  service: Service = {} as Service
+  protected servicesList: Service[] = [];
+  // bookService: BookService = inject(BookService);
+  readingForm: ReadingFormComponent | undefined;
 
-  constructor(private bookService: BookService) {}
+  constructor(private bookService: BookService) {
+    
+  }
 
   ngOnInit(): void {
-    this.bookService.getServiceDetails().subscribe({
-      // Question: How can I change this from any to Service?
-      next: (service: any) => {
-        console.log(`in events asking for service ${service.name}`)
-        this.service = service;
-      },
-      error: (error) => {
-        console.log(error);
-      },
-    });
+    // this.servicesList = this.bookService.getAllServicesDetails();
+    // console.log(this.servicesList);
+
+    const data = this.bookService.service;
+    console.log(data);
+
+    // const navigation = this.router.getCurrentNavigation();
+    // const appointment = navigation?.extras?.state?.['services'];
+
+    // console.log('State:', navigation?.extras?.state);
   }
 
   calendarOptions: CalendarOptions = {
