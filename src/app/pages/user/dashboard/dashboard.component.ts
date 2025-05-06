dacd8b451f33f4099cd68cc03fd6a19b39b0bc05
quinise <quinise.ercolano@gmail.com>
2025-05-06 15:36:34 -0700

    Shows both past and upcoming appointments in the AppointmentHistory Component in the user Dashboard.

--- src/app/pages/user/dashboard/dashboard.component.ts
+++ src/app/pages/user/dashboard/dashboard.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppUser } from 'src/app/interfaces/appUser';
 import { AuthService } from 'src/app/services/authentication/auth.service';
+import { AppointmentHistoryComponent } from '../appointment-history/appointment-history.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [NgIf, AsyncPipe],
+  imports: [NgIf, AsyncPipe, AppointmentHistoryComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
