e5c40623de92cf3f748a7ca2c1089d6a4a6cdba4
quinise <quinise.ercolano@gmail.com>
2025-05-05 19:28:40 -0700

    Added a welcome message in the dashboard

--- src/app/interfaces/appUser.ts
+++ src/app/interfaces/appUser.ts
@@ -1,5 +1,6 @@
 export interface AppUser {
   uid: string;
+  displayName: string;
   email: string;
   role: 'admin' | 'user' | string;
 }
--- src/app/pages/user/dashboard/dashboard.component.ts
+++ src/app/pages/user/dashboard/dashboard.component.ts
@@ -1,20 +1,19 @@
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { take } from 'rxjs';
+import { Observable } from 'rxjs';
+import { AppUser } from 'src/app/interfaces/appUser';
 import { AuthService } from 'src/app/services/authentication/auth.service';
+
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [NgIf, AsyncPipe],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  isUser: boolean = false;
+  user$: Observable<AppUser | null>;
 
-  constructor(private authService: AuthService) {}
-  
-    ngOnInit(): void {
-      this.authService.user$.pipe(take(1)).subscribe(user => {
-        this.isUser = user?.role === 'user';
-      });
-    }
+  constructor(private authService: AuthService) {
+    this.user$ = this.authService.user$;
+  }
 }
