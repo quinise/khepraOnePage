25e9636c2e0f1423f2d45a405374669a2b82abcd
quinise <quinise.ercolano@gmail.com>
2025-05-07 09:37:02 -0700

    added delete appointment functionality to the Appointment History component

--- src/app/pages/user/appointment-history/appointment-history.component.ts
+++ src/app/pages/user/appointment-history/appointment-history.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AsyncPipe, NgFor, NgIf, DatePipe } from '@angular/common';
-import { Observable, of, combineLatest } from 'rxjs';
-import { switchMap, map  } from 'rxjs/operators';
+import { Observable, of, BehaviorSubject } from 'rxjs';
 import { Appointment } from 'src/app/interfaces/appointment';
 import { AuthService } from 'src/app/services/authentication/auth.service';
 import { AppointmentApiService } from 'src/app/services/apis/appointmentApi.service';
-
+import { DeleteEventService } from 'src/app/services/delete-event.service';
 @Component({
   selector: 'app-appointment-history',
   standalone: true,
@@ -15,20 +14,39 @@ import { AppointmentApiService } from 'src/app/services/apis/appointmentApi.serv
 })
 export class AppointmentHistoryComponent implements OnInit {
   pastAppointments$: Observable<Appointment[]> = of([]);
-  upcomingAppointments$: Observable<Appointment[]> = of([]);
+  upcomingAppointments$ = new BehaviorSubject<Appointment[]>([]);
+  upcomingAppointments: Appointment[] = [];
   
   constructor(
     private authService: AuthService,
-    private appointmentApiService: AppointmentApiService
+    private appointmentApiService: AppointmentApiService,
+    private deleteService: DeleteEventService,
   ) {}
 
   ngOnInit(): void {
     this.authService.user$.subscribe(user => {
       if (user?.uid) {
-        this.upcomingAppointments$ = this.appointmentApiService.getAppointmentsByUserId(user.uid, 'upcoming');
+        this.appointmentApiService.getAppointmentsByUserId(user.uid, 'upcoming').subscribe(appointments => {
+          this.upcomingAppointments = appointments;
+          this.upcomingAppointments$.next(this.upcomingAppointments);
+        });
+
         this.pastAppointments$ = this.appointmentApiService.getAppointmentsByUserId(user.uid, 'past');
       }
     });
   }
   
+  deleteAppointment(id: number): void {
+    this.deleteService.deleteAppointment(
+      id,
+      this.upcomingAppointments,
+      {},
+      () => {
+        // After deletion, update BehaviorSubject so the view refreshes
+        this.upcomingAppointments$.next([...this.upcomingAppointments]);
+        // TODO: Success action - show a toast
+        console.log('Appointment deleted from history component');
+      }
+    );
+  }
 }
