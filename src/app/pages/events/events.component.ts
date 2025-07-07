3e44e159076cf0a8d8d490af498d5c8aabc3cd9b
quinise <quinise.ercolano@gmail.com>
2025-07-06 18:57:48 -0700

    updated calendar, events, and panel styles

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,16 +1,22 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { CalendarViewComponent } from 'src/app/components/shared/calendar-view/calendar-view.component';
+import { AuthWrapperService } from 'src/app/services/authentication/auth-wrapper.service';
 @Component({
   selector: 'app-events',
-  imports: [CalendarViewComponent],
+  imports: [NgClass, CalendarViewComponent],
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css'],
 })
 export class EventsComponent {
+  constructor(private authWrapperService: AuthWrapperService) {}
+
+  userId: string = this.authWrapperService.getCurrentUser()?.uid || '';;
+  
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin]
+    plugins: [dayGridPlugin],
   };
 }
